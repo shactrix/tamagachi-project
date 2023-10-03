e329f3892315a5ca663ebcd0b121cfce3aa5d96d
Jon Reven <jonmountainboy@gmail.com>
2023-10-02 20:02:55 -0400

    set interval and inner html

--- tama.js
+++ tama.js
@@ -19,56 +19,41 @@ nameId.innerHTML = `${petName}`
     // Feed the Tamagotchi, decreasing hunger
 function getHungry() {
     this.hunger--;
+    document.getElementById("foodLevel").innerHTML = newPet.hunger
     if (this.hunger <= 5) {
         alert('FEED ME!')
     } else if(newPet.hunger === 0){
         alert(`${petName} starved to death :C `)
     }
-}
+} setInterval(getHungry, 1000)
 
 // Put the Tamagotchi to sleep, decreasing sleepiness
 function getSleepy() {
     this.sleepiness--;
+    document.getElementById("sleepLevel").innerHTML = newPet.sleepiness
     if (this.sleepiness <= 5) {
         alert(`I'm tired of this grandpa!!!`)
     } else if(newPet.sleepiness === 0){
         alert(`${petName} died of exhaustion :C `)
     }
-}
+} setInterval(getSleepy, 1000)
 
 // Play with the Tamagotchi, decreasing boredom
 function getBored() {
     this.boredom--;
+    document.getElementById("boredLevel").innerHTML = newPet.boredom
     if (this.boredom <= 5) {
         alert('I am so very very very bored.')
     } else if(newPet.boredom === 0){
         alert(`${petName} died of boredom :C `)
     }
-}
+} setInterval(getBored, 1000)
 
 // Increase the age of the Tamagotchi
 function increaseAge() {
     this.age++;
     console.log(`${petName.name} has aged. Age: ${petName.age}`);
-}
-
-
-
-// Display initial metrics
-console.log(`${myTamagotchi.name}'s Metrics:`);
-console.log(`Hunger: ${myTamagotchi.hunger}`);
-console.log(`Sleepiness: ${myTamagotchi.sleepiness}`);
-console.log(`Boredom: ${myTamagotchi.boredom}`);
-console.log(`Age: ${myTamagotchi.age}`);
-
-// Interact with the Tamagotchi
-myTamagotchi.feed();
-myTamagotchi.sleep();
-myTamagotchi.play();
-myTamagotchi.increaseAge();
-//   This code defines a Tamagotchi class with methods to feed, put to sleep, play with, and increase the age of the Tamagotchi. You can create an instance of the class and interact with it to see how its metrics change.
-
-
+} setInterval(increaseAge, 3000)
 
 
 })
